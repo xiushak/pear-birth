6d90987c1a6856e698854ad166e79780762e075c
xiushak <xiebrandon@gmail.com>
2025-03-11 00:59:35 -0700

    updated some stufff for basic format

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,4 @@
+export function Footer() {
+  // idk, i just know kk likes feet
+  return <footer className="px-32 py-8 text-center">FOOTER</footer>;
+}
---
+++ src/components/navbar/index.tsx
@@ -0,0 +1,4 @@
+export function NavBar() {
+  // set ur birthday here or seomthing?
+  return <footer className="px-32 py-8 text-center">NAVBAR</footer>;
+}
