d0cc0a6d8bcd0a03db1c203fcdfc2dae968a8e99
xiushak <xiebrandon@gmail.com>
2025-03-10 21:28:14 -0700

    idk some starter code or something

---
+++ src/constants/index.ts
@@ -0,0 +1,3 @@
+// date set in ISO 8601 "year-month-date T hour:minute:seconds:milliseconds Z"
+export const PEAR_BIRTH = new Date("2001-12-26T08:00:00.000Z");
+export const FORMATTED_PEAR_BIRTH = PEAR_BIRTH.toLocaleDateString();
